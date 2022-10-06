a684cae7bc1937c9c9ef2b7fc3181b7e3b740d78
osmaralves182 <osmar.alves@outlook.com>
2022-10-06 05:33:02 -0300

    Lista dinâmica e Arrays

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace console_desafio21dias_api
   {
     static void Main(string[] args)
     {
+
       // Aula 3 - Loop | Minuto: 33 Minutos
       Console.WriteLine("Digite o número inicial: ");
       int numeroInicial = Convert.ToInt32(Console.ReadLine());
@@ -36,6 +37,44 @@ namespace console_desafio21dias_api
       foreach (var item in itens)
       {
         Console.WriteLine(item);
+      }
+
+      // Não é tão usada
+      /*string [][] alunos = new string [5][5];
+
+      alunos[0][0] = "Osmar";
+      alunos[1][0] = "123";
+      alunos[2][0] = "10, 9, 8";*/
+
+      List<dynamic> alunos = new List<dynamic>();
+
+      alunos.Add(new
+      {
+        Nome = "Osmar",
+        Matricula = "001",
+        Notas = "10,9,8"
+      });
+
+      alunos.Add(new
+      {
+        Nome = "Maria",
+        Matricula = "001",
+        Notas = "10,9,8"
+      });
+
+      alunos.Add(new
+      {
+        Nome = "Sheila",
+        Matricula = "001",
+        Notas = "10,9,8"
+      });
+
+      foreach (var aluno in alunos)
+      {
+        Console.WriteLine($"Nome do aluno: {aluno.Nome}");
+        Console.WriteLine($"Matrícula do aluno: {aluno.Matricula}");
+        Console.WriteLine($"Notas do aluno: {aluno.Notas}");
+        Console.WriteLine($"---------------------------------");
       }
 
       //Aula 1 e 2
